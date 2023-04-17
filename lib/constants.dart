4a1fe1604ce3a5b2504d48f8dcccaef140964e0b
yashprovis <yash.bhansali@provistechnologies.com>
2023-04-17 14:00:18 +0530

    rawg apk

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,2 +1,1 @@
-const String rawgKey = String.fromEnvironment('RAWG_KEY',
-    defaultValue: '64623cc51a1d4bb897c2463cfd231477');
+const String rawgKey = String.fromEnvironment('RAWG_KEY', defaultValue: '');
